40c9d9fde4a58d05674efe5edfb5f420a4a03dec
luoweixian <luoweixian@appgame.com>
2017-02-05 14:58:39 +0800

    Site updated: 2017-02-05 14:58:39

--- js/main.js
+++ js/main.js
@@ -50,7 +50,7 @@ $(document).ready(function() {
     $('.btn-mobile-menu__icon').toggleClass('fa fa-list fa fa-angle-up animated fadeIn');
   });
 
-  $("article.post-container--single a[href^=http]").attr("target", "_blank");
+  $("article.post-container--single a[href^=https]").attr("target", "_blank");
   $("article.post-container--single a[href^=mailto]").attr("target", "_blank");
   
   
